0fe1914e9393ecab61c009b2efdf59639b6dec2f
kingsunjinking <mrin33@naver.com>
2024-03-26 14:19:41 +0900

    240326

--- src/main/java/Quiz.java
+++ src/main/java/Quiz.java
@@ -8,29 +8,25 @@ public class Quiz {
 
         int num = s.nextInt();
         int i;
-        if (num % 2 == 0) {
-            for (i = 1; i <= num / 2 + 1; i++) {
-                for (int q = num / 2 + 1; q > i; q--) {
-                    System.out.print(" ");
-                }
-
-                for (int j = 1; j <= i * 2 - 1; j++) {
+            if (num % 2 == 0) {
+                for (i = 1; i <= num / 2 + 1; i++) {
+                    for (int q = num / 2 + 1; q > i; q--) {
+                        System.out.print(" ");
+                    }
+                    for (int j = 1; j <= i * 2 - 1; j++) {
                     System.out.print("*");
-                }
+                    }
                 System.out.println();
             }
-            System.out.print("짝수는 안돼서 1을 더했습니다.");}
-
-        if (num % 2 != 0) {
-            for (i = 1; i <= (num + 1) / 2; i++) {
-                for (int q = (num + 1) / 2; q > i; q--) {
-                    System.out.print(" ");
-                }
-
-                for (int j = 1; j <= i * 2 - 1; j++) {
+                System.out.print("짝수는 안돼서 1을 더했습니다.");}
+            if (num % 2 != 0) {
+                for (i = 1; i <= (num + 1) / 2; i++) {
+                    for (int q = (num + 1) / 2; q > i; q--) {
+                        System.out.print(" ");
+                    }
+                    for (int j = 1; j <= i * 2 - 1; j++) {
                     System.out.print("*");
-                }
-
+                    }
                 System.out.println();
             }
         }
