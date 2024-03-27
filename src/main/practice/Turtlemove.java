d54b25cd509418107603846c383af250563d1876
kingsunjinking <mrin33@naver.com>
2024-03-27 17:54:21 +0900

    240327

---
+++ src/main/practice/Turtlemove.java
@@ -0,0 +1,20 @@
+public class Turtlemove {
+    public static void main(String[] args){
+        Turtle turtle = new Turtle();
+
+        turtle.width(10);
+        turtle.penColor("yellow");
+
+        turtle.forward(200);
+        turtle.left(144);
+        turtle.forward(400);
+        turtle.left(144);
+        turtle.forward(400);
+        turtle.left(144);
+        turtle.forward(400);
+        turtle.left(144);
+        turtle.forward(400);
+        turtle.left(144);
+        turtle.forward(200);
+    }
+}
