d8d05388b007560064b196b9b40e07882f87e106
kingsunjinking <mrin33@naver.com>
2024-03-27 18:04:56 +0900

    240327

---
+++ src/main/practice/Doublestar.java
@@ -0,0 +1,42 @@
+public class Doublestar {
+    public static void main(String[] args){
+        Turtle turtle = new Turtle();
+
+        turtle.width(10);
+        turtle.penColor("yellow");
+
+        turtle.forward(50);
+        turtle.left(144);
+        turtle.forward(100);
+        turtle.left(144);
+        turtle.forward(100);
+        turtle.left(144);
+        turtle.forward(100);
+        turtle.left(144);
+        turtle.forward(100);
+        turtle.left(144);
+        turtle.forward(150);
+        turtle.right(36);
+        turtle.forward(100);
+        turtle.right(144);
+        turtle.forward(100);
+        turtle.right(144);
+        turtle.forward(100);
+        turtle.right(144);
+        turtle.forward(100);
+        turtle.right(144);
+        turtle.forward(150);
+        turtle.right(36);
+        turtle.forward(100);
+        turtle.left(144);
+        turtle.forward(100);
+        turtle.left(144);
+        turtle.forward(100);
+        turtle.left(144);
+        turtle.forward(100);
+        turtle.left(144);
+        turtle.forward(100);
+
+    }
+}
+
