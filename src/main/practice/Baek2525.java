a6244ca79b4dfed4c684d740bbb3b2f58b03eec7
kingsunjinking <mrin33@naver.com>
2024-03-28 20:44:16 +0900

    240328

--- src/main/practice/Baek2525.java
+++ src/main/practice/Baek2525.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Baek2525 {
-    public static void main(String[] args) {
+    public static void main(String[] args){
 
         Scanner s = new Scanner(System.in);
 
