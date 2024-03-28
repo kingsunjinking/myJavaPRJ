029bd006afbfc47850fc69a6b6a0812f3208bc85
kingsunjinking <mrin33@naver.com>
2024-03-28 19:00:59 +0900

    240328

---
+++ src/main/practice/Beak2884.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Beak2884 {
+    public static void main(String[] args){
+
+        Scanner s = new Scanner(System.in);
+        int h = s.nextInt(); int m = s.nextInt();
+
+        int w = 45-m;
+        int e = 60-w;
+        int r = h-1;
+
+        if(m>=45){
+            int q = m-45;
+            System.out.printf("%d %d",h, q);
+        }
+        else if(h==0 && m < 45){
+            System.out.printf("23 %d",e);
+        }
+        else{
+            System.out.printf("%d %d",r,e);
+        }
+    }
+}
