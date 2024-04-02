1a76dace2379986902be569de7c0ed5c0cd09028
kingsunjinking <mrin33@naver.com>
2024-04-02 13:53:54 +0900

    240402

---
+++ src/main/practice/Baek11022.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Baek11022 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int t = s.nextInt();
+
+        for(int i = 1; i<=t; i++){
+            int a= s.nextInt(); int b=s.nextInt();
+            System.out.println("Case #" + i + ": " + a + " + " + b + " = " +(a+b));
+        }
+    }
+}
+
