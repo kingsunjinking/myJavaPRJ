6bfcf12cd510e898f66963724c22eb621713fb63
kingsunjinking <mrin33@naver.com>
2024-04-08 09:25:31 +0900

    240408

---
+++ src/main/practice/Chapter2/Baek9498.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Baek9498 {
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+
+        {
+            if (a <= 100 && a >= 90) {
+                System.out.print("A");
+            }
+            else if (a <= 89 && a >= 80) {
+                System.out.print("B");
+            }
+            else if (a <= 79 && a >= 70) {
+                System.out.print("C");
+            }
+            else if (a <= 69 && a >= 60) {
+                System.out.print("D");
+            }
+            else{
+                System.out.print("F");
+            }
+        }
+    }
+}
