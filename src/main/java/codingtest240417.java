36174815a616268215fa4bce555f5657162e1cdd
kingsunjinking <mrin33@naver.com>
2024-04-17 17:07:24 +0900

    240417

--- src/main/java/codingtest240417.java
+++ src/main/java/codingtest240417.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Zcodingtest240416 {
+public class codingtest240417 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int a = s.nextInt();
