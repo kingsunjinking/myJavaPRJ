e117d4ab944bed137ee6f039e9a2250d9620a961
kingsunjinking <mrin33@naver.com>
2024-04-19 16:18:18 +0900

    240419

--- src/main/java/CharAtPractice.java
+++ src/main/java/CharAtPractice.java
@@ -1,11 +1,11 @@
 public class CharAtPractice {
     public static void main(String[] args){
-        String str = "127.0.0.1 - frank [10/Oct/2000:13:55:36 -0700] \"GET /apache_pb.gif HTTP/1.0\" 200 2326 \"http://www.example.com/start.html\" \"Mozilla/4.08 [en] (Win98; I ;Nav)\"";
-        int a=str.indexOf(" ");
-        System.out.println(str.substring(0,a).trim());
+        String log = "127.0.0.1 - frank [10/Oct/2000:13:55:36 -0700] \"GET /apache_pb.gif HTTP/1.0\" 200 2326 \"http://www.example.com/start.html\" \"Mozilla/4.08 [en] (Win98; I ;Nav)\"";
+        int c=log.indexOf(" ");
+        System.out.println(log.substring(0,c).trim());
 
-        int b=str.indexOf("\"");
-        int x=str.indexOf(" ",b);
-                System.out.println(str.substring(b+1,x));
+        int a = log.indexOf("\"");
+        int b = log.indexOf(" ",a);
+        System.out.println(log.substring(a+1,b));
     }
 }
