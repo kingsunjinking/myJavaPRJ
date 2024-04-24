1022fabf35bcf8b788adbf008568715380e06c14
kingsunjinking <mrin33@naver.com>
2024-04-24 16:11:31 +0900

    240424

---
+++ src/main/practice/Chapter5/Baek11654.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Baek11654 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        char a = s.nextLine().charAt(0);
+        int b = (int)a;
+
+        System.out.println(b);
+    }
+}
