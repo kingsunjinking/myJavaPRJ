b773c2fe70a9688c2a9bda26df18312171af5ad0
kingsunjinking <mrin33@naver.com>
2024-05-17 15:52:32 +0900

    240517

---
+++ src/main/practice/Baek/Baek1094.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Baek1094 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+        int b = 64;
+        int c = b/2;
+        int count = 0;
+        System.out.println(count);
+    }
+}
+
