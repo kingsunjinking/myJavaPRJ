223189de5cf8fa923e525449c8ab50f1920e11d2
kingsunjinking <mrin33@naver.com>
2024-06-10 21:01:26 +0900

    240610

---
+++ src/main/practice/Baek/Baek1225.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Baek1225 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        String a = s.nextLine();
+        String b = s.nextLine();
+        int c = 0;
+        for (int i = 0; i < a.length(); i++) {
+            for (int j = 0; j < b.length(); j++) {
+                char d = a.charAt(i);
+                char e = b.charAt(j);
+
+                c += Integer.parseInt(String.valueOf(d)) * Integer.parseInt(String.valueOf(e));
+            }
+        }
+
+        System.out.println(c);
+    }
+}
