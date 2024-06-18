df03a31c31740bd7662e24c8a1df355d5b6d0604
kingsunjinking <mrin33@naver.com>
2024-06-18 15:06:20 +0900

    240618

---
+++ src/main/practice/Baek/Baek14581.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Baek14581 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        String a = s.next();
+
+        System.out.println(":fan::fan::fan:\n" +
+                ":fan::"+a+"::fan:\n" +
+                ":fan::fan::fan:");
+    }
+}
