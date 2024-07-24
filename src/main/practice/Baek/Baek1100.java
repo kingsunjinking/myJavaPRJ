009e52f06bda70e12b2ed7bd8a8dd86985afa13e
kingsunjinking <mrin33@naver.com>
2024-07-24 13:23:23 +0900

    240724

---
+++ src/main/practice/Baek/Baek1100.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Baek1100 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+
+        int count = 0;
+        boolean color = false;
+
+        for (int i = 0; i < 8; i++) {
+            String a = s.nextLine();
+            if (color == true) {
+                color = false;
+            } else if (color == false) {
+                color = true;
+            }
+            for (int j = 0; j < 8; j++) {
+                if (a.charAt(j) == 'F' && color == true) {
+                    count++;
+                }
+
+                if (color == true) {
+                    color = false;
+                } else if (color == false) {
+                    color = true;
+                }
+            }
+        }
+        System.out.println(count);
+    }
+}
