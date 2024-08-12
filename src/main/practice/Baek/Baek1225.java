5b295a42adbfca48741497143b2f210704a8e83b
kingsunjinking <mrin33@naver.com>
2024-08-12 19:21:25 +0900

    240812

--- src/main/practice/Baek/Baek1225.java
+++ src/main/practice/Baek/Baek1225.java
@@ -4,9 +4,9 @@ public class Baek1225 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
 
-        String a = s.nextLine();
-        String b = s.nextLine();
-        int c = 0;
+        String a = s.next();
+        String b = s.next();
+        long c = 0;
         for (int i = 0; i < a.length(); i++) {
             for (int j = 0; j < b.length(); j++) {
                 char d = a.charAt(i);
