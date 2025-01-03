03487e4e7b2f3f7192737bc87e43e5f743bbe961
kingsunjinking <mrin33@naver.com>
2025-01-03 18:12:27 +0900

    240103

--- src/main/practice/Chapter6/Baek1316.java
+++ src/main/practice/Chapter6/Baek1316.java
@@ -1,21 +1,35 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Baek1316 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
+
         int a = s.nextInt();
+
         int count = 0;
+
         for (int i = 0; i < a; i++) {
-            String st  = s.nextLine();
-            for (int j = 0; j < st.length(); j++) {
-                if(j==st.length()-1){
-                    break;
+            boolean chk = false;
+            boolean [] arr = new boolean[26];
+            Arrays.fill(arr,false);
+            String b = s.next();
+            for (int j = 0; j < b.length(); j++) {
+                if (j != 0){
+                    if(b.charAt(j) != b.charAt(j-1)){
+                        if (arr[(int) b.charAt(j) - 97] == true) {
+                            chk = true;
+                            break;
+                        }
+                    }
                 }
-                else if(st.charAt(j) == st.charAt(j+1)){
-                    count++;
-                    break;
-                }
+                arr[(int)b.charAt(j)-97] = true;
             }
-        }System.out.println(count);
+            if(chk == false){
+                count++;
+            }
+        }
+
+        System.out.println(count);
     }
 }
