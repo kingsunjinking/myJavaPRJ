eeda6d928722a42ea8fe60629e601d696cab1db7
kingsunjinking <mrin33@naver.com>
2025-02-28 18:01:38 +0900

    250228

---
+++ src/main/practice/Baek/Baek1076.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Baek1076 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String [] arr = new String[3];
+        String a = "";
+        int b;
+        for (int i = 0; i < 3; i++) {
+            arr[i] = s.nextLine();
+        }
+
+        switch (arr[0]){
+
+        }
+    }
+}
